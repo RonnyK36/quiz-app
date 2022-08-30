af13d8e4e6365f08cd9ca3434fb8efba6bfb52e7
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-30 23:56:33 +0300

    Iterate over all available questions

--- app.js
+++ app.js
@@ -32,3 +32,34 @@ const quizData = [{
         answer: 'b',
     },
 ]
+
+
+const questionEl = document.getElementById('question')
+const aText = document.getElementById('a-text')
+const bText = document.getElementById('b-text')
+const cText = document.getElementById('c-text')
+const dText = document.getElementById('d-text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuestion = 0
+loadQuestion()
+
+function loadQuestion() {
+    const currentQuiz = quizData[currentQuestion]
+    questionEl.innerText = currentQuiz.question
+    aText.innerText = currentQuiz.a
+    bText.innerText = currentQuiz.b
+    cText.innerText = currentQuiz.c
+    dText.innerText = currentQuiz.d
+    currentQuestion++
+
+}
+submitBtn.addEventListener("click", () => {
+    if (currentQuestion !== quizData.length) {
+        loadQuestion()
+    } else {
+        currentQuestion = 0
+    }
+
+})
