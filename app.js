3c5b7fdd62e0ae484802a240379c543a2fea7672
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-30 23:18:12 +0300

    Created sample questions

---
+++ app.js
@@ -0,0 +1,34 @@
+const quizData = [{
+        question: 'Who is the president of Kenya?',
+        a: 'Moi',
+        b: 'Odinga',
+        c: 'Kanye',
+        d: 'Ruto',
+
+        answer: 'b',
+    },
+    {
+        question: 'What color is milk?',
+        a: 'red',
+        b: 'green',
+        c: 'white',
+        d: 'colorless',
+        answer: 'c',
+    },
+    {
+        question: 'Which is not even number?',
+        a: '1',
+        b: '224',
+        c: '1738',
+        d: '16',
+        answer: 'a',
+    },
+    {
+        question: 'Where was Jesus born?',
+        a: 'Jerusalem',
+        b: 'Bethlehem',
+        c: 'Jericho',
+        d: 'Canada',
+        answer: 'b',
+    },
+]
