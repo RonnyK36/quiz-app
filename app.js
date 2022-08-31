f9bb9442ef2e5264a0cc83a4ce688aca2f1b961f
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-31 23:46:23 +0300

    Completed Quiz app to track answers and show scores with dynamic message in each result

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const quizData = [{
         c: 'Kanye',
         d: 'Ruto',
 
-        answer: 'b',
+        answer: 'd',
     },
     {
         question: 'What color is milk?',
@@ -40,26 +40,98 @@ const bText = document.getElementById('b-text')
 const cText = document.getElementById('c-text')
 const dText = document.getElementById('d-text')
 const submitBtn = document.getElementById('submit')
+const showScore = document.getElementById('score')
+
+
+const answersEl = document.querySelectorAll(".answer")
 
 
 let currentQuestion = 0
 loadQuestion()
+let score = 0
+
+
+
 
 function loadQuestion() {
+
+    deselectChoices()
+
     const currentQuiz = quizData[currentQuestion]
     questionEl.innerText = currentQuiz.question
     aText.innerText = currentQuiz.a
     bText.innerText = currentQuiz.b
     cText.innerText = currentQuiz.c
     dText.innerText = currentQuiz.d
-    currentQuestion++
+
 
 }
+
+function getSelectedAnswer() {
+
+
+    let answer = undefined
+    answersEl.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+function deselectChoices() {
+    answersEl.forEach((answerEl) => {
+        answerEl.checked = false
+    });
+}
+
 submitBtn.addEventListener("click", () => {
-    if (currentQuestion !== quizData.length) {
-        loadQuestion()
+
+    const answer = getSelectedAnswer()
+    const currentQuiz = quizData[currentQuestion]
+
+    if (answer) {
+        if (answer === currentQuiz.answer) {
+            score++
+            console.log(score);
+        } else {
+            console.log(score);
+            console.log(answer);
+            console.log(currentQuiz.answer);
+        }
+        currentQuestion++
+
+
+
+        if (currentQuestion !== quizData.length) {
+            loadQuestion()
+        } else {
+            let remarks = ''
+            score === 4 ? remarks = 'Excellent Score' :
+                score === 3 ? remarks = 'Good' : score === 2 ? remarks = 'Average' :
+                remarks = 'Try Harder'
+            showScore.innerHTML = `<h4>Your Score! 🤩😎</h4><h2>${score}/${quizData.length}</h2><p>${remarks}</p>`
+            showScore.classList.add('show-result')
+
+        }
     } else {
-        currentQuestion = 0
+
     }
 
+
+
 })
+
+function checkCorrectAns(selected) {
+    const currentQuiz = quizData[currentQuestion]
+    console.log(selected);
+    console.log(currentQuiz.answer);
+    if (selected === currentQuiz.answer) {
+
+        console.log("Correct");
+
+    } else {
+        console.log("Wrong");
+    }
+
+}
